ac76f2a82115885f7698032492993b8a0a29622c
Onji Kim <xodks672@gmail.com>
2023-03-20 10:47:32 +0900

    [ADD] default spring boot 구조 추가
    JDK version = 11
    spring boot version = 2.7.7
    
    application.yml
    
    created from spring ininitializr
    https://start.spring.io/

---
+++ src/test/java/com/semtleWebGroup/youtubeclone/YoutubeCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semtleWebGroup.youtubeclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
