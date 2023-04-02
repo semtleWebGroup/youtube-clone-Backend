bc174938b33c4224f047d70da35c54b076aa6335
hj-language <idgafff010@gmail.com>
2023-04-02 19:06:33 +0900

    [UPDATE] title NotNull 대신 NotEmpty로 변경

--- src/main/java/com/semtleWebGroup/youtubeclone/domain/video/dto/VideoRequest.java
+++ src/main/java/com/semtleWebGroup/youtubeclone/domain/video/dto/VideoRequest.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
@@ -13,7 +14,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class VideoRequest {
-    @NotNull(message="'title' cannot be null.")
+    @NotEmpty(message="'title' cannot be empty.")
     @Size(max=45, message="'title' length should be <= 45.")
     private String title;
 
