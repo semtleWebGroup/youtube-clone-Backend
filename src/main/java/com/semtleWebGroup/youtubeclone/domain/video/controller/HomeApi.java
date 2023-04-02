c874e04350043d9a9ff0e4aebfa319c211e2a019
hj-language <idgafff010@gmail.com>
2023-04-02 17:08:51 +0900

    [ADD] Home mockup api 작성

---
+++ src/main/java/com/semtleWebGroup/youtubeclone/domain/video/controller/HomeApi.java
@@ -0,0 +1,32 @@
+package com.semtleWebGroup.youtubeclone.domain.video.controller;
+
+import com.semtleWebGroup.youtubeclone.domain.video.dto.VideoListResponse;
+import com.semtleWebGroup.youtubeclone.domain.video.service.VideoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/home")
+public class HomeApi {
+    private final VideoService videoService;
+
+    @GetMapping("")
+    public ResponseEntity getList() {
+        // TODO
+        ArrayList<VideoListResponse> videos = new ArrayList<VideoListResponse>();
+        for (int i = 0; i < 20; i++) {
+            videos.add(
+                    new VideoListResponse("title" + i, "channel" + i, i, i)
+            );
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(videos);
+    }
+}
