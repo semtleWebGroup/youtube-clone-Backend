d004cb784d46bf296deb45986ecfc858d779c85f
Onji Kim <xodks672@gmail.com>
2023-04-05 23:05:57 +0900

    [FIX] Revert HotFix
    너무 꼬여서 예찬님 커밋은 최대한 리버트 시켰습니다.
    최대한 확인한다고 했으나, 본인 도메인 정상 작동 한번씩 확인해주세요

--- src/main/java/com/semtleWebGroup/youtubeclone/domain/channel/repository/SubscribeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.semtleWebGroup.youtubeclone.domain.channel.repository;
-
-import com.semtleWebGroup.youtubeclone.domain.channel.domain.Subscribe;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubscribeRepository extends JpaRepository<Subscribe, Integer> {
-}
--- src/main/java/com/semtleWebGroup/youtubeclone/domain/video/repository/VideoLikeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.semtleWebGroup.youtubeclone.domain.video.repository;
-
-import com.semtleWebGroup.youtubeclone.domain.video.domain.VideoLike;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VideoLikeRepository extends JpaRepository<VideoLike, Integer> {
-}
