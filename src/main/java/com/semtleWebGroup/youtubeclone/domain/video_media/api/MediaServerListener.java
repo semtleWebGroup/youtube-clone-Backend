b77d50583a77b6010dfcf69da4f5517bd0546389
Onji Kim <xodks672@gmail.com>
2023-05-01 19:44:42 +0900

    [REFACTOR] refactoring db and media domain
    서버 분리에 따라 도메인 수정

---
+++ src/main/java/com/semtleWebGroup/youtubeclone/domain/video_media/api/MediaServerListener.java
@@ -0,0 +1,12 @@
+package com.semtleWebGroup.youtubeclone.domain.video_media.api;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 미디어 서버의 이벤트를 받아 처리해주는 컨트롤러 <br>
+ * 내부용 컨트롤러이므로 외부에서 호출되지 않음
+ */
+@Controller
+public class MediaServerListener {
+
+}
