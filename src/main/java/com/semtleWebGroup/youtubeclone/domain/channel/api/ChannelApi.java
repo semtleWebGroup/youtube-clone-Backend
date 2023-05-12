a945bd561a755fb1a8b1cc5fe556826e747debe9
ShinYeaChan <88381563+YeaChan05@users.noreply.github.com>
2023-05-12 15:23:20 +0900

    [UPDATE] SecurityContextHolder에 Authentication 객체 확인을 위한 로그 추가

--- src/main/java/com/semtleWebGroup/youtubeclone/domain/channel/api/ChannelApi.java
+++ src/main/java/com/semtleWebGroup/youtubeclone/domain/channel/api/ChannelApi.java
@@ -10,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -29,6 +31,10 @@ public class ChannelApi {
      */
     @PostMapping("")
     public ResponseEntity create(@ModelAttribute ChannelRequest dto){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Object principal = authentication.getPrincipal();
+        log.info("authentication 내의 member id: {}",principal.toString());
+        
         Channel channel = channelService.addChannel(dto);
         ChannelDto res = Optional.ofNullable(channel).map(ChannelDto::new).orElse(null);
 
