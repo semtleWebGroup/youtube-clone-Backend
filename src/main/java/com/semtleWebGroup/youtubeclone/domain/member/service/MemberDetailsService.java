1e8ae090e586c1a6fe4c6a556d1e2b0ec02fb3ca
ShinYeaChan <88381563+YeaChan05@users.noreply.github.com>
2023-05-12 15:15:06 +0900

    [UPDATE] UserDetails 반환 객체를 db에서 가져온 객체로 변경

--- src/main/java/com/semtleWebGroup/youtubeclone/domain/member/service/MemberDetailsService.java
+++ src/main/java/com/semtleWebGroup/youtubeclone/domain/member/service/MemberDetailsService.java
@@ -2,28 +2,29 @@ package com.semtleWebGroup.youtubeclone.domain.member.service;
 
 import com.semtleWebGroup.youtubeclone.domain.member.domain.Member;
 import com.semtleWebGroup.youtubeclone.domain.member.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class MemberDetailsService implements UserDetailsService {
     
     @Autowired
     private MemberRepository memberRepository;
     
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<Member> member = memberRepository.findByName(username);
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<Member> member = memberRepository.findByEmail(email);
         if(member.isEmpty()) {
-            throw new UsernameNotFoundException(username);
+            //TODO: 예외처리 조정
+            throw new UsernameNotFoundException(email);
         }
-        return new User(member.get().getName(),member.get().getPassword(),new ArrayList<>());
+        return member.get();
     }
 }
