318c3b4e36c40760f058bf560505eb4033c8dd5c
hj-language <idgafff010@gmail.com>
2023-05-16 01:15:04 +0900

    Revert "[UPDATE] sql문 변경"
    This reverts commit d238414168a4bf145503c1bba2335ec3958a9c6b.

--- database.sql
+++ database.sql
@@ -52,18 +52,18 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `youtube_clone`.`subscribe` (
   `subscribe_id` BIGINT(20) NOT NULL,
-  `subscriber_id` BIGINT(20) NOT NULL,
-  `channel_id` BIGINT(20) NOT NULL,
+  `subscriber` BIGINT(20) NOT NULL,
+  `channel` BIGINT(20) NOT NULL,
   PRIMARY KEY (`subscribe_id`),
-  INDEX `fk_subscribe_channel1_idx` (`subscriber_id` ASC) VISIBLE,
-  INDEX `fk_subscribe_channel2_idx` (`channel_id` ASC) VISIBLE,
+  INDEX `fk_subscribe_channel1_idx` (`subscriber` ASC) VISIBLE,
+  INDEX `fk_subscribe_channel2_idx` (`channel` ASC) VISIBLE,
   CONSTRAINT `fk_subscribe_channel1`
-    FOREIGN KEY (`subscriber_id`)
+    FOREIGN KEY (`subscriber`)
     REFERENCES `youtube_clone`.`channel` (`channel_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_subscribe_channel2`
-    FOREIGN KEY (`channel_id`)
+    FOREIGN KEY (`channel`)
     REFERENCES `youtube_clone`.`channel` (`channel_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
@@ -82,8 +82,7 @@ CREATE TABLE IF NOT EXISTS `youtube_clone`.`video` (
   `updated_time` DATETIME NOT NULL,
   `view_count` INT NOT NULL,
   `channel_id` BIGINT(20) NOT NULL,
-  `video_sec` BIGINT DEFAULT 0,
-  `status` ENUM('PUBLIC', 'PRIVATE', 'DRAFT') NOT NULL,
+  `status` ENUM('public', 'private', 'draft') NOT NULL,
   PRIMARY KEY (`video_id`),
   INDEX `fk_video_info_channel1_idx` (`channel_id` ASC) VISIBLE,
   CONSTRAINT `fk_video_info_channel1`
