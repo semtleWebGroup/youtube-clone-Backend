c07b5f71563ce7977cf541599f1d50fa28876bad
Onji Kim <xodks672@gmail.com>
2023-05-26 18:39:58 +0900

    [FIX] 미디어 서버 통신 시, 로그 찍히도록 수정

--- src/main/java/com/semtleWebGroup/youtubeclone/domain/video_media/service/ProdMediaServerSpokesman.java
+++ src/main/java/com/semtleWebGroup/youtubeclone/domain/video_media/service/ProdMediaServerSpokesman.java
@@ -5,14 +5,9 @@ import com.semtleWebGroup.youtubeclone.domain.video_media.dto.request.VodInitPos
 import com.semtleWebGroup.youtubeclone.domain.video_media.dto.response.GetEncodingStatusResponse;
 import com.semtleWebGroup.youtubeclone.domain.video_media.dto.response.VodInitPostResponse;
 import com.semtleWebGroup.youtubeclone.global.error.exception.MediaServerException;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.*;
-import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.lang.Nullable;
-import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
@@ -55,26 +50,11 @@ public class ProdMediaServerSpokesman implements MediaServerSpokesman {
                     httpRequest,
                     VodInitPostResponse.class);
         } catch (HttpClientErrorException e){
-            //To-Do 예외처리
-            throw new MediaServerException();
+            log.error(e.getLocalizedMessage());
+            throw new MediaServerException(e);
         }
-        log.debug("response from media server: {}", response);
-
-        //응답 확인
-        HttpStatus statusCode = response.getStatusCode();
-
-        if (response.getBody() == null) {
-            //To-Do 예외 처리
-            throw new MediaServerException();
-        } else if (statusCode == HttpStatus.OK) {
-            return response.getBody().getVideoLength();
-        } else if (statusCode == HttpStatus.BAD_REQUEST) {
-            //To-Do 예외처리
-            throw new MediaServerException();
-        } else {
-            //To-Do 예외처리
-            throw new MediaServerException();
-        }
+        log.debug("response from media server: {}", response.getStatusCodeValue());
+        return response.getBody().getVideoLength();
     }
 
     @Override
@@ -86,24 +66,11 @@ public class ProdMediaServerSpokesman implements MediaServerSpokesman {
         try {
             response = restTemplate.exchange(url, HttpMethod.GET, HttpEntity.EMPTY, GetEncodingStatusResponse.class);
         } catch (HttpClientErrorException e){
-            //To-Do 예외처리
-            throw new MediaServerException();
+            log.error(e.getLocalizedMessage());
+            throw new MediaServerException(e);
         }
-
-        HttpStatus statusCode = response.getStatusCode();
-
-        if (response.getBody() == null) {
-            //To-Do 예외 처리
-            throw new MediaServerException();
-        } else if (statusCode == HttpStatus.OK) {
-            return response.getBody().getEntireJobStatus();
-        } else if (statusCode == HttpStatus.BAD_REQUEST) {
-            //To-Do 예외처리
-            throw new MediaServerException();
-        } else {
-            //To-Do 예외처리
-            throw new MediaServerException();
-        }
+        log.debug("response from media server: {}", response.getStatusCodeValue());
+        return response.getBody().getEntireJobStatus();
     }
 
     @Override
@@ -116,21 +83,11 @@ public class ProdMediaServerSpokesman implements MediaServerSpokesman {
         try {
             response = restTemplate.exchange(url, HttpMethod.DELETE, HttpEntity.EMPTY, Void.class);
         } catch (HttpClientErrorException e){
-            //To-Do 예외처리
-            throw new MediaServerException();
+            log.error(e.getLocalizedMessage());
+            throw new MediaServerException(e);
         }
+        log.debug("response from media server: {}", response.getStatusCodeValue());
 
-        HttpStatus statusCode = response.getStatusCode();
-
-        if (statusCode == HttpStatus.OK) {
-            return;
-        } else if (statusCode == HttpStatus.BAD_REQUEST) {
-            //To-Do 예외처리
-            throw new MediaServerException();
-        } else {
-            //To-Do 예외처리
-            throw new MediaServerException();
-        }
 
     }
 
@@ -147,21 +104,10 @@ public class ProdMediaServerSpokesman implements MediaServerSpokesman {
         try {
             response = restTemplate.exchange(url, HttpMethod.PATCH, body, Void.class);
         } catch (HttpClientErrorException e){
-            //To-Do 예외처리
-            throw new MediaServerException();
+            log.error(e.getLocalizedMessage());
+            throw new MediaServerException(e);
         }
-
-        HttpStatus statusCode = response.getStatusCode();
-
-        if (statusCode == HttpStatus.OK) {
-            return;
-        } else if (statusCode == HttpStatus.BAD_REQUEST) {
-            //To-Do 예외처리
-            throw new MediaServerException();
-        } else {
-            //To-Do 예외처리
-            throw new MediaServerException();
-        }
+        log.debug("response from media server: {}", response.getStatusCodeValue());
 
 
 
